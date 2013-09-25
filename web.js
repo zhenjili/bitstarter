b01ab694e3ac42ff51ef76573cd8758c1217be82
jie xu <jiex@uci.edu>
2013-09-25 19:03:49 +0000

    read message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf =  fs.readFileSync('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
